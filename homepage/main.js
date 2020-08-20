bb925bb1a2192c655af75e6086c1d18a565064ce
Taras Ivanyk <hanekava4@gmail.com>
2020-08-20 18:12:55 +0200

    czwartek 20.08

--- homepage/main.js
+++ homepage/main.js
@@ -1,1 +1,6 @@
-alert('Hi:\)');
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+    const navigation = document.querySelector('.header__navigation');
+    navigation.classList.toggle('show');
+});
